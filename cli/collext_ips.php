5b43a7988b3e2f31d2a94d6310fecb73456f6ec3
Renaat <rdebleu@eWallah.net>
2020-02-18 11:35:53 +0000

    ips

---
+++ cli/collext_ips.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This script checks online user map works.
+ *
+ * @package   block_online_users_map
+ * @copyright 2019 onwards eWallah
+ * @author    Renaat Debleu (www.ewallah.net)
+ * @author    Alex Little
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+define('CLI_SCRIPT', true);
+
+require(__DIR__ . '/../../../config.php');
+require_once($CFG->libdir . '/clilib.php');
+require_once($CFG->dirroot . '/blocks/online_users_map/lib.php');
+
+cli_heading('Checking ips');
+update_users_locations();
